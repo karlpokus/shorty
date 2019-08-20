f9be450f6a34824b20379520be41d191072bb43e
Karl Pokus <karl.pokus@gmail.com>
2019-08-20 14:05:51 +0200

    update short id sequence

--- shorty.go
+++ shorty.go
@@ -1,23 +1,24 @@
 package shorty
 
 import (
-  "math/rand"
-  "time"
+	"math/rand"
+	"time"
 )
 
-// charCodes: 65 -> 90 A-Z, 97 -> 122 a-z
+// plan B with charCodes: 65 -> 90 A-Z, 97 -> 122 a-z
 var charpool = []byte("abcdefghijklmnopqrstuvxyz1234567890")
 
-func randChar() byte {
+// randByte returns a random char from charpool
+func randByte() byte {
 	return charpool[rand.Intn(len(charpool))]
 }
 
-func RandSeq(n int) []byte {
-  rand.Seed(time.Now().UnixNano())
-
-  var out []byte
-  for i := 0; i < n; i++ {
-    out = append(out, randChar())
-  }
-  return out
+// randSeq returns n random bytes from charpool
+func randSeq(n int) []byte {
+	rand.Seed(time.Now().UnixNano())
+	var out []byte
+	for i := 0; i < n; i++ {
+		out = append(out, randByte())
+	}
+	return out
 }
